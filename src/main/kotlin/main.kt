cbb1b85441ce4b6a23a9278db95358bec46f843d
art_spb@hotmail.it <Privet2100+>
2020-05-11 16:43:49 +0200

    л

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,6 @@
 import com.galaktionov.firstandroidapp.dto.Location
 import com.galaktionov.firstandroidapp.dto.Post
 import com.google.gson.Gson
-import com.sun.jndi.toolkit.url.Uri
 
 infix fun Double.x(that: Double) = Location(lat = this, long = that)
 
@@ -13,44 +12,7 @@ public fun main() {
 }
 
 private fun prepareList(): MutableList<Post> = mutableListOf(
-    Post(
-        1L,
-        "Netology",
-        "Funny cats!",
-        1597796778000,
-        true,
-        2,
-        0,
-        1823,
-        videoUrl = "https://www.youtube.com/embed/WEkSYw3o5is",
-        postTpe = Post.POST_TYPE.VIDEO
-    ),
-    Post(
-        1L,
-        "Google",
-        "Фестиваль книги",
-        1587711778000,
-        false,
-        20,
-        100,
-        300,
-        "Nevsky Prospect",
-        59.932030 x 30.355610,
-        postTpe = Post.POST_TYPE.EVENT,
-        companyImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png"
-    ),
-    Post(
-        1L,
-        "Twitter",
-        "Twitter post",
-        1587796778000,
-        false,
-        250,
-        10,
-        111,
-        postTpe = Post.POST_TYPE.REPLY,
-        companyImg = "https://upload.wikimedia.org/wikipedia/it/0/09/Twitter_bird_logo.png"
-    ),
+
     Post(
         1L,
         "Google",
@@ -65,18 +27,29 @@ private fun prepareList(): MutableList<Post> = mutableListOf(
         companyImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png"
     ),
     Post(
-        1L,
-        "Facebook",
-        "Funny cats pt.2 ",
-        1587567778000,
-        false,
-        16,
-        12,
-        3,
-        null,
-        null,
-        videoUrl = "https://www.youtube.com/embed/hY7m5jjJ9mM",
-        postTpe = Post.POST_TYPE.VIDEO,
-        companyImg = "https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512"
+        15L,
+        "Google",
+        "Try the best search engine!",
+        1523496778000,
+        true,
+        5,
+        1,
+        0,
+        postTpe = Post.POST_TYPE.ADV,
+        advLink = "http://www.google.com",
+        companyImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png"
+    ),
+    Post(
+        10L,
+        "Google",
+        "Try the best search engine!",
+        1523496778000,
+        true,
+        5,
+        1,
+        0,
+        postTpe = Post.POST_TYPE.ADV,
+        advLink = "http://www.google.com",
+        companyImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png"
     )
 )
